f42da5c9a0ade9a7762643355db40bfb29e06d82
M4RKL4B0Y <markvlaboy@gmail.com>
2023-12-15 21:56:54 -0500

    save

--- public/app.js
+++ public/app.js
@@ -1,9 +1,53 @@
 //Note: You do NOT need to create an external JS file for this project despite any instructions you may see in Canvas. This file, app.js, is the only external JS file you need.
 
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 12,
+});
+
+// Add OpenStreetMap tiles:
+// L.tileLayer('https://.tile.openstreetmap.org///.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//     minZoom: '15',
+// }).addTo(myMap)
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap);
+
+
+// Create and add a geolocation marker:
+
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
+
+
 
 
 // Create map
 
+// var myMap = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+L.marker([51.5, -0.09]).addTo(myMap)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+
+// Draw the 2nd Arrondissement    
+
+var polygon = L.polygon([
+[48.863368120198004, 2.3509079846928516],
+[48.86933262048345, 2.3542531602919805],
+[48.87199261164275, 2.3400569901592183],
+[48.86993336274516, 2.3280142476578813], 
+[48.86834104280146, 2.330308418109664]
+]).addTo(myMap);
 
 
 
